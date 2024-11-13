511b67f08491829daa030539234471df4cca7ad9
Дарья <grbnv.di@gmail.com>
2024-11-13 11:01:09 +0300

    Add files via upload
    поправлены названия переменных и улучшен импорт данных

--- import_data/data_import.py
+++ import_data/data_import.py
@@ -46,14 +46,14 @@ def get_google_sheet_data(spreadsheet_name):
     worksheets = sheet.worksheets()
 
     data_frames = {}
-    ugly_v_grad = []  # Список для хранения данных из второго столбца первого листа
-    nested_list_2 = []  # Список для хранения данных из второго столбца второго листа
-    third_column_first_sheet = []  # Список для хранения данных из третьего столбца первого листа
-    # Список для хранения данных из второго столбца второго листа
-    third_column_list_a = []  # Список для хранения первой половины третьего столбца второго листа
-    third_column_list_b = []  # Список для хранения второй половины третьего столбца второго листа
-    fourth_column_list_a = []
-    fourth_column_list_b = []  # Список для второй половины четвертого столбца второго листа
+    ugly_v_grad = []
+    direct = []
+    prol = []
+
+    x_nach = []
+    x_kon = []
+    y_nach = []
+    y_kon = []
 
     for i, worksheet in enumerate(worksheets):
         # Получаем все данные из листа
@@ -67,11 +67,11 @@ def get_google_sheet_data(spreadsheet_name):
                 try:
                     numbers = list(map(int, value.split(',')))  # Преобразуем строки в целые числа
                     if len(numbers) == 3:  # Проверяем, что у нас три числа
-                        ugly_v_grad.append(numbers[0]+numbers[1]/60+numbers[2]/3600)  # Добавляем в составной список
+                        ugly_v_grad.append(numbers[0] + numbers[1]/60 + numbers[2]/3600)  # Добавляем в составной список
                 except ValueError:
                     continue  # Игнорируем ошибки преобразования
             # Извлечение третьего столбца, игнорируя последнее значение
-            third_column_first_sheet = df.iloc[:, 2].tolist()  # Сохраняем третий столбец в список без последнего значения
+            prol = df.iloc[:, 2].tolist()# Сохраняем третий столбец в список без последнего значения
 
         elif i == 1:  # Проверяем, является ли это вторым листом
             # Извлечение второго столбца и преобразование в списки с тремя целыми числами
@@ -79,38 +79,38 @@ def get_google_sheet_data(spreadsheet_name):
                 try:
                     numbers = list(map(int, value.split(',')))  # Преобразуем строки в целые числа
                     if len(numbers) == 3:  # Проверяем, что у нас три числа
-                        nested_list_2.append(numbers)  # Добавляем в составной список
+                        direct.append(numbers)  # Добавляем в составной список
                 except ValueError:
                     continue
 
             # Извлечение третьего столбца из второго листа
             third_column_second_sheet = df.iloc[:, 2].tolist()  # Сохраняем третий столбец во список
-            # Разделяем nested_list_2 на два списка
-            mid_index = len(nested_list_2) // 2
-            nested_list_2a = nested_list_2[:mid_index]  # Первый список
-            nested_list_2b = nested_list_2[mid_index:]  # Второй список
+            # Разделяем direct на два списка
+            mid_index = len(direct) // 2
+            direct_n = direct[:mid_index]  # Первый список
+            direct_k = direct[mid_index:]  # Второй список
             # Разделяем третий столбец на два разных списка
             mid_index = len(third_column_second_sheet) // 2
-            third_column_list_a = third_column_second_sheet[:mid_index]  # Первый список
-            third_column_list_b = third_column_second_sheet[mid_index:]  # Второй список
+            x_nach = third_column_second_sheet[:mid_index]  # Первый список
+            x_kon = third_column_second_sheet[mid_index:]  # Второй список
             # Извлечение четвертого столбца из второго листа
             fourth_column_second_sheet = df.iloc[:, 3].tolist()  # Сохраняем четвертый столбец во список
             # Разделяем четвертый столбец на два разных списка
             mid_index_fourth = len(fourth_column_second_sheet) // 2
-            fourth_column_list_a = fourth_column_second_sheet[:mid_index_fourth]  # Первый список
-            fourth_column_list_b = fourth_column_second_sheet[mid_index_fourth:]  # Второй список
+            y_nach = fourth_column_second_sheet[:mid_index_fourth]  # Первый список
+            y_kon = fourth_column_second_sheet[mid_index_fourth:]  # Второй список
 
         data_frames[worksheet.title] = df
 
     logging.info(f"... выполнен успешно.")
 
-    return data_frames, ugly_v_grad, nested_list_2a, nested_list_2b, third_column_first_sheet, third_column_list_a, third_column_list_b, fourth_column_list_a, fourth_column_list_b  # Возвращаем DataFrames и списки
+    return data_frames, ugly_v_grad, direct_n, direct_k, prol, x_nach, x_kon, y_nach, y_kon  # Возвращаем DataFrames и списки
 
 
 def enother_get_data(spreadsheet_name: str):
-    dataframes,  ugly_v_grad, _, __, prologenie, *lishnie_dannye = get_google_sheet_data(spreadsheet_name)
+    dataframes,  ugly_v_grad, _, __,prol, *lishnie_dannye = get_google_sheet_data(spreadsheet_name)
     dataframes["sheet1"]["Десятичный угол"] = ugly_v_grad
-    dataframes["sheet1"]["Проложение в метрах"] = prologenie
+    dataframes["sheet1"]["Проложение в метрах"] = prol
     dataframes["sheet1"].drop(columns=['Измеренные углы', 'Горизонт прол'], inplace=True)
 
     logging.info(f"Передаваемые данные: {dataframes}")
@@ -132,7 +132,7 @@ def enother_get_data(spreadsheet_name: str):
 if __name__ == '__main__':
     spreadsheet_name = 'project'  # Замените на название вашей таблицы
     ddd = enother_get_data(spreadsheet_name)
-    data, ugly_v_grad, nested_list_2a, nested_list_2b, third_column_values, third_column_list_a, third_column_list_b, fourth_column_list_a, fourth_column_list_b = get_google_sheet_data(spreadsheet_name)
+    data, ugly_v_grad, direct_n, direct_k, prol, x_nach, x_kon, y_nach, y_kon = get_google_sheet_data(spreadsheet_name)
 
     # Выводим данные из каждого листа
     for title, df in data.items():
@@ -140,24 +140,5 @@ if __name__ == '__main__':
         print(df)
         print("\n")  # Пустая строка для разделения данных разных листов
 
-    # Выводим вложенные списки из второго столбца первого и второго листов
-    print("Измеренные углы:")
-    print(ugly_v_grad, "\n")
-    print("Дирекционный углол (начальный):")
-    print(nested_list_2a)
-    print("Дирекционный углол (конечный):")
-    print(nested_list_2b, "\n")
-    print("Горизонтальные проложения:")
-    print(third_column_values, "\n")
-    print("Координаты X исходных точек:")
-    print("X первой точки:")
-    print(third_column_list_a)
-    print("X конечной точки:")
-    print(third_column_list_b, "\n")
-    print("Координаты Y исходных точек:")
-    print("Y первой точки:")
-    print(fourth_column_list_a)
-    print("Y конечной точки:")
-    print(fourth_column_list_b)
 
     print(f"{ddd=}")
